3e37c907573f506eabdae7ad2e25edf7fb339398
Joseph Luna <josephluna@Josephs-MBP.local>
2019-06-29 13:11:19 -0500

    inital commit

---
+++ friends/src/reducer/index.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+const defaultState = {
+        friends:[]
+}
+
+
+export const rootReducer = (state = defaultState, action) => {
+    switch (action.type){
+
+        default: 
+            return state;
+    }
+}
